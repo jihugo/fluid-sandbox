a21af11c50a770b652dd7dd817392c106fdedc3c
hugo <jiyuge@outlook.com>
2025-01-16 20:28:55 -0600

    commit before rename

--- physics.py
+++ physics.py
@@ -2,19 +2,32 @@
 
 from typing import Optional, Iterable
 
+import numpy as np
+
 
 class Cell:
     """Cell in space"""
 
-    __slots__ = ["exists", "weight"]
+    __slots__ = [
+        "exists",
+        "weight",
+        "velocity",
+        "acceleration",
+        "pressure",
+        "num_neighbors",
+    ]
 
     def __init__(self, exists: bool, weight: float = 0) -> None:
         self.exists = exists
         self.weight = weight
+        self.velocity = np.array([0, 0])
+        self.acceleration = np.array([0, 0])
+        self.pressure = 0
+        self.num_neighbors = 0
 
     def __repr__(self) -> str:
         return (
-            " "
+            "◦"
             if not self.exists
             else (
                 "■"
@@ -22,6 +35,11 @@ class Cell:
                 else "▤" if self.weight > 0.5 else "▪" if self.weight > 0.2 else "▫"
             )
         )
+    
+    def 
+
+    def run() -> None:
+        """One step of the simulation"""
 
 
 class CV:
@@ -29,7 +47,7 @@ class CV:
 
     __slots__ = ["grid"]
 
-    def __init__(self, grid = None):
+    def __init__(self, grid=None):
         self.grid: Optional[Iterable] = grid
 
     def __repr__(self) -> str:
@@ -45,7 +63,20 @@ class CVRectangle(CV):
     def __init__(self, width: float, height: float, resolution: float):
         grid_width = int((width + 0.5 * resolution) / resolution)
         grid_height = int((height + 0.5 * resolution) / resolution)
-        grid = [
-            [Cell(True) for _ in range(grid_width)] for _ in range(grid_height)
-        ]
+        grid = np.concat(
+            [
+                [[Cell(False) for _ in range(grid_width + 2)]],
+                [
+                    np.concat(
+                        [
+                            [Cell(False)],
+                            [Cell(True) for _ in range(grid_width)],
+                            [Cell(False)],
+                        ]
+                    )
+                    for _ in range(grid_height)
+                ],
+                [[Cell(False) for _ in range(grid_width + 2)]],
+            ]
+        )
         super().__init__(grid)
